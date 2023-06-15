d76a46dfc22435b04bb8cd38d935e3c6cbfa92f7
Xinoo0 <ed.fernandezr@duocuc.cl>
2023-06-14 22:58:59 -0400

    Version 0.1

---
+++ WebTaller/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Auto, Cliente
+
+# Register your models here.
+
+
+admin.site.register(Auto)
+admin.site.register(Cliente)
---
+++ WebTaller/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Auto(models.Model):
+    patente = models.CharField(primary_key=True, max_length=6 )
+    marca   = models.CharField(max_length=20)
+    modelo  = models.CharField(max_length=20)
+    año     = models.DateField(blank=False, null=False)
+
+
+class Cliente(models.Model):
+    rut    = models.CharField(primary_key=True, max_length=10)
+    nombre = models.CharField(max_length=20)
+    ap_paterno = models.CharField(max_length=20)
+    ap_materno = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField(blank=False, null=False)
+    telefono = models.CharField(max_length=45)
+    email = models.CharField(unique=True, max_length=100, blank=True, null=True)
+    direccion = models.CharField(max_length=50, blank=True, null=True)
