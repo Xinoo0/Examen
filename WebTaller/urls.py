f45a054749204bf6b59da1b25b5f2819e52ca6b8
Xinoo0 <ed.fernandezr@duocuc.cl>
2023-06-17 12:25:28 -0400

    Version 0.3

--- WebTaller/urls.py
+++ WebTaller/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
 
 
 urlpatterns = [
+
     path('index', views.index, name='index'),
+
+    # GESTION USUARIOS
+    path('login', views.login, name='login'),
+    path('salir', views.salir, name='salir'),
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- WebTaller/views.py
+++ WebTaller/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Auto
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 
 # Create your views here.
 
 
 def index(request):
     context={}
-    return render(request, 'web/index.html', context)
+    return render(request, 'Web/index.html', context)
+
+
+# GESTION USUARIOS
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+def salir(request):
+    logout(request)
+    return redirect('/')
